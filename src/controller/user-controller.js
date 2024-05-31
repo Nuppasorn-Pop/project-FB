e656673656ad3394983f1947c651a9ec10d6eb7e
Nuppasorn-Pop <nuppasorn.s@gmail.com>
2024-05-31 10:14:38 +0700

    uploade image in cloudinary and remove image from my disk

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,18 +1,34 @@
+const fs = require("fs/promises");
+
 const userService = require("../services/user-service");
 const uploadService = require("../services/upload-service");
 
 const userController = {};
 userController.updateProfileOrCoverImage = async (req, res, next) => {
   try {
-    const data = {}; // ต้องการให้ผลลัพธ์เป็น {profileImage: path, coverImage: path} เพื่อจะได้ส่งไป upload ที่ database
+    const promises = [];
     if (req.files.profileImage) {
-      data.profileImage = req.files.profileImage[0].path;
-      const result = await uploadService.upload(req.files.profileImage[0].path);
-      console.log(result);
+      const result = uploadService
+        .upload(req.files.profileImage[0].path)
+        .then((url) => ({ url, key: "profileImage" }));
+      promises.push(result);
     }
     if (req.files.coverImage) {
-      data.coverImage = req.files.coverImage[0].path;
+      const result = uploadService
+        .upload(req.files.coverImage[0].path)
+        .then((url) => ({ url, key: "coverImage" }));
+      promises.push(result);
     }
+
+    const result = await Promise.all(promises);
+    // promises = [promise1,promise2]
+    // allResult ===> [resolvePromise1,resolvepromise2] or [resolvePromise1] or [resolvepromise2]
+    console.log(result);
+    const data = result.reduce((acc, el) => {
+      acc[el.key] = el.url;
+      return acc;
+    }, {});
+    // ต้องการให้ผลลัพธ์เป็น {profileImage: path, coverImage: path} เพื่อจะได้ส่งไป upload ที่ database
 
     await userService.updateUserById(data, req.user.id);
     res.status(200).json(data);
@@ -25,6 +41,13 @@ userController.updateProfileOrCoverImage = async (req, res, next) => {
     // res.json("usercontroller");
   } catch (err) {
     next(err);
+  } finally {
+    if (req.files.profileImage) {
+      fs.unlink(req.files.profileImage[0].path);
+    }
+    if (req.files.coverImage) {
+      fs.unlink(req.files.coverImage[0].path);
+    }
   }
 };
 
